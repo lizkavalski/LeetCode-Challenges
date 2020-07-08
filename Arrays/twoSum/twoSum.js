5fec59bdb73b4db12d7be6be571471c721ce165d
lizkavalski <lizkavalski@gmail.com>
2020-07-08 14:51:56 -0700

    small edits

--- Arrays/twoSum/twoSum.js
+++ Arrays/twoSum/twoSum.js
@@ -1,12 +1,22 @@
 'use strict';
 var twoSum = function(nums, target) {
-  let map = {};
-  for(let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    const complement = map[target - num];
-    if (complement !== undefined) {
-      return [i, complement];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-    map[num] = i;
   }
 };
+
+// with a hashmap
+// var twoSum = function(nums,target){
+//   const indices = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     const complement = target - nums[i];
+//     if (indices.has(complement)) {
+//       return [indices.get(complement), i];
+//     }
+//     indices.set(nums[i], i);
+//   }
+// }
